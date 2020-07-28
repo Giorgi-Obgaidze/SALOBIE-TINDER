42853a1f3b25c0b4507946ab18efdaf3973759e9
Giorgi-Obgaidze <gobga17@freeuni.edu.ge>
2020-07-28 12:50:01 +0400

    minor fixes

--- LoveFindERRZ/web/MyWebPage.js
+++ LoveFindERRZ/web/MyWebPage.js
@@ -1,5 +1,6 @@
 var id = 0;
 var images = [];
+var availableID = [];
 function imagePreview() {
 
     var preview = document.querySelector('#preview');
@@ -16,43 +17,50 @@ function imagePreview() {
             var reader = new FileReader();
 
             reader.addEventListener("load", function () {
-                var image = document.createElement("IMG");
+                if(id >= 6){
+                    return alert("image limit has been reached");
+                }else {
+                    var image = document.createElement("IMG");
                     //new Image();
-                var close = document.createElement("button");
-                var loc_id = id;
-                image.id = "image" + loc_id.toString();
-                close.id = "button" + loc_id.toString();
-                id++;
-                image.height = 200;
-                image.width = 300;
-                image.title = file.name;
-                image.src = this.result;
-                image.style.padding = "10px";
-                images.push(image);
+                    var close = document.createElement("button");
+                    var loc_id;
+                    if (availableID.length > 0) {
+                        let loc_id = availableID.pop();
+                    } else {
+                        let loc_id = id;
+                    }
+                    image.id = "image" + loc_id.toString();
+                    close.id = "button" + loc_id.toString();
+                    id++;
+                    image.height = 200;
+                    image.width = 300;
+                    image.title = file.name;
+                    image.src = this.result;
+                    image.style.padding = "10px";
+                    images.push(image);
 
-                //var close = document.createElement('span');
-                close.style.color = "#aaa";
-                close.style.float = "right";
-                close.style.fontSize = "26px";
-                close.style.fontWeight = "bold";
-                preview.appendChild(close);
-                preview.appendChild( image );
-                close.onclick = function(){
-                    console.log(image.id);
-                    console.log(close.id);
-                    //console.log("button" + image.id.substr(5));
-                    var notNeeded = document.getElementById(image.id);
-                    var suicideButton = document.getElementById(close.id);
-                    notNeeded.parentNode.removeChild(notNeeded);
-                    suicideButton.parentNode.removeChild(suicideButton);
-                    id--;
+                    //var close = document.createElement('span');
+                    close.style.color = "#aaa";
+                    close.style.float = "right";
+                    close.style.fontSize = "26px";
+                    close.style.fontWeight = "bold";
+                    preview.appendChild(close);
+                    preview.appendChild(image);
+                    close.onclick = function () {
+                        var notNeeded = document.getElementById(image.id);
+                        var suicideButton = document.getElementById(close.id);
+                        availableID.push(image.id);
+                        notNeeded.parentNode.removeChild(notNeeded);
+                        suicideButton.parentNode.removeChild(suicideButton);
+                        document.removeChild(notNeeded);
+                        document.removeChild(suicideButton);
+                    }
+                    //image.appendChild(close);
+                    // preview.appendChild(close);
+                    // preview.appendChild( image );
                 }
-                //image.appendChild(close);
-                // preview.appendChild(close);
-                // preview.appendChild( image );
             });
-
-            reader.readAsDataURL(file);
+                reader.readAsDataURL(file);
         }else{
             return alert("File type is not supported or it is not an image!")
         }
